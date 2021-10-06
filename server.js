4352f398f8ce7a8d03a5551a352a8fa57232b791
Andrew Durham <andrewdurham1094@gmail.com>
2021-10-06 10:49:46 -0600

    began building the server

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-const fs = require('fs')
-const path = require('path')
 const express = require('express')
 const PORT = process.env.PORT || 3002
 const app = express()
 
-app.use(express.static('public'))
-
-app.use(express.urlencoded({ extended: true }))
-
-app.use(express.json())
-app.use('/api', apiRoutes)
-app.use('/', htmlRoutes)
-
 const { notes } = require('./db/db.json')
 
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray
+    if (query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if (query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text)
+    }
+    return filteredResults
+}
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results)
+    }
+    res.json(results)
+})
 
 app.listen(PORT, () => {
     console.log(`API server not on port ${PORT}!`)
